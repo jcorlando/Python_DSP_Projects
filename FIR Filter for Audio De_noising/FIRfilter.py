9a6dfd332c8a650c33e3046dc6fb28f8d835f3ee
Juan Orlando <jcorlandomc@yahoo.com>
2021-05-20 00:23:35 -0700

    05/20/2021 12:23 am

--- FIR Filter for Audio De_noising/FIRfilter.py
+++ FIR Filter for Audio De_noising/FIRfilter.py
@@ -4,6 +4,10 @@ import sys
 import matplotlib.pyplot as plt
 
 signal = wave.open('imperial_march_noisy.wav', 'rb')
+sampleRate = signal.getframerate()
+ampWidth = signal.getsampwidth()
+nChannels = signal.getnchannels()
+nFrames = signal.getnframes()
 
 raw = signal.readframes(-1)
 raw = np.frombuffer(raw, "int16")
@@ -11,6 +15,8 @@ raw = np.frombuffer(raw, "int16")
 fs = signal.getframerate()
 
 dt = np.linspace(0, len(raw) / fs, num=len(raw))
+
+#  Plot Time Domain Signal
 plt.figure(1)
 plt.title("Time Domain Plot")
 plt.plot(dt, raw, color="blue")
@@ -19,6 +25,8 @@ plt.show()
 
 
 
+
+# Plot Frequency Domain 
 plt.figure(2)
 
 
@@ -26,10 +34,7 @@ plt.figure(2)
 
 
 
-sampleRate = signal.getframerate()
-ampWidth = signal.getsampwidth()
-nChannels = signal.getnchannels()
-nFrames = signal.getnframes()
+
 filtered = wave.open("filtered.wav", "w")
 filtered.setparams((1, ampWidth, sampleRate, nFrames, signal.getcomptype(), signal.getcompname()))
 filtered.writeframes(raw.tobytes('C'))
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Python_DSP_Projects 
-# Python_DSP_Projects 
