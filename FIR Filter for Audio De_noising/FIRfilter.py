2bee458d11942ead4491d74ad34e1dee0658016a
Juan Orlando <jcorlandomc@yahoo.com>
2021-05-22 00:53:30 -0700

    05/22/2021 12:52 am

--- FIR Filter for Audio De_noising/FIRfilter.py
+++ FIR Filter for Audio De_noising/FIRfilter.py
@@ -2,9 +2,11 @@ import wave
 import numpy as np
 import sys
 import math
+import struct
 import matplotlib.pyplot as plt
 from scipy import signal
 from scipy.signal import lfilter, butter, kaiserord, firwin, freqz
+from scipy.io import wavfile
 
 # Use website below for documentaion of creating FIR Filters
 # https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.remez.html
@@ -12,9 +14,10 @@ from scipy.signal import lfilter, butter, kaiserord, firwin, freqz
 #------Open Noisy Signal file and set Parameters equal to variables------
 x = wave.open('imperial_march_noisy.wav', 'rb')
 sampleRate = x.getframerate()
-ampWidth = x.getsampwidth()
+sampWidth = x.getsampwidth()
 nChannels = x.getnchannels()
 nFrames = x.getnframes()
+
 #------Open Noisy Signal file and set Parameters equal to variables------
 
 #-----------Extract Raw Data from wav file--------------
@@ -26,33 +29,34 @@ raw = np.frombuffer(raw, "int16")
 dt = np.linspace(0, len(raw) / sampleRate, num=len(raw))
 #------------------Define and set x-axis------------------------
 
-#---------------Plot Time Domain Signal of Original Signal---------------
+#---------------Plot Time Domain of Original Signal---------------
 # plt.figure()
 # plt.title("Time Domain Plot")
 # plt.plot(dt, raw, color="blue")
 # plt.xlabel("Time")
-#---------------Plot Time Domain Signal of Original Signal---------------
+#---------------Plot Time Domain of Original Signal---------------
 
 #------------------Compute the fft------------------
 y = np.fft.fft(raw)
 freq = np.fft.fftfreq(dt.shape[-1], 1/sampleRate)
 #------------------Compute the fft------------------
 
-#--------------Plot FFT Frequency Domain of Original Signal---------------
+#--------------Plot FFT Frequency Response of Original Signal---------------
 # plt.figure()
 # plt.title("Frequency Response of Signal")
 # plt.plot(freq, abs(y))
+# plt.xlim(-270, 5700)
+# plt.ylim(-4.1E7, 1.949E9)
 # plt.xlabel("Frequency")
 # plt.ylabel('Magnitude')
-#--------------Plot FFT Frequency Domain of Original Signal---------------
-
+#--------------Plot FFT Frequency Response of Original Signal---------------
 
 
 
 #---------------------------Create Band Stop Filter------------------------------
 band = [1106, 1150]  # Desired stop band, Hz
-trans_width = 600    # Width of transition from pass band to stop band, Hz
-numtaps = 175        # Size of the FIR filter.
+trans_width = 12     # Width of transition from pass band to stop band, Hz
+numtaps = 3101       # Size of the FIR filter.
 edges = [0, band[0] - trans_width, band[0], band[1], band[1] + trans_width, 0.5*sampleRate]
 taps = signal.remez(numtaps, edges, [1, 0, 1], Hz=sampleRate)
 w, h = signal.freqz(taps, [1], worN=2000)
@@ -60,34 +64,53 @@ w, h = signal.freqz(taps, [1], worN=2000)
 
 
 #----------------Plot Frequency Response of filter----------------
-plt.figure()
-plt.plot(0.5*sampleRate*w/np.pi, 20*np.log10(np.abs(h)))
-plt.ylim(-40, 5)
-plt.xlim(0, 0.5*sampleRate)
-plt.grid(True)
-plt.xlabel('Frequency (Hz)')
-plt.ylabel('Gain (dB)')
-plt.title("Band-stop Filter")
+# plt.figure()
+# plt.plot(0.5*sampleRate*w/np.pi, 20*np.log10(np.abs(h)))
+# plt.ylim(-66.5, 8)
+# plt.xlim(530, 1800)
+# plt.grid(True)
+# plt.xlabel('Frequency (Hz)')
+# plt.ylabel('Gain (dB)')
+# plt.title("Band-stop Filter")
 #----------------Plot Frequency Response of filter----------------
 
 
+#--------------Apply First Filter to the Original Signal------------------
+raw_filtered = lfilter(taps, [1], raw)
+raw_filtered = raw_filtered.astype('int16')
+#--------------Apply First Filter to the Original Signal------------------
 
 
-#--------------Filter the Original Signal------------------
+#------------------Compute the fft------------------
+y = np.fft.fft(raw_filtered)
+freq = np.fft.fftfreq(dt.shape[-1], 1/sampleRate)
+#------------------Compute the fft------------------
 
-#--------------Filter the Original Signal------------------
+#--------------Plot FFT Frequency Response of Filtered Signal---------------
+# plt.figure()
+# plt.title("Frequency Response of Signal")
+# plt.plot(freq, abs(y))
+# plt.xlim(-270, 5700)
+# plt.ylim(-4.1E7, 1.949E9)
+# plt.xlabel("Frequency")
+# plt.ylabel('Magnitude')
+#--------------Plot FFT Frequency Response of Filtered Signal---------------
 
 
-
-
+#---------------Plot Time Domain of Filtered Signal---------------
+# plt.figure()
+# plt.title("Time Domain Plot Filtered Signal")
+# plt.plot(dt, raw_filtered, color="blue")
+# plt.xlabel("Time")
+#---------------Plot Time Domain of Filtered Signal---------------
 
 #-------Show all Plots and Figures-------
 plt.show()
 #-------Show all Plots and Figures-------
 
 #-----------------Write Filtered file to new file-----------------
-filtered = wave.open("filtered.wav", "w")
-filtered.setparams( (1, ampWidth, sampleRate, nFrames, x.getcomptype(), x.getcompname()) )
-filtered.writeframes(raw.tobytes('C'))
+filtered = wave.open("filtered.wav", "wb")
+filtered.setparams( (nChannels, sampWidth, sampleRate, nFrames, x.getcomptype(), x.getcompname()) )
+filtered.writeframes(raw_filtered.tobytes('C'))
 filtered.close()
 #-----------------Write Filtered file to new file-----------------
